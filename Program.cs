de9b70cb30f92ae5246c79497263dec50dd2863f
Felipe Souza <67338813+flpesouza@users.noreply.github.com>
2023-10-03 15:53:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+
+namespace Exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            int cont = 0;
+
+            Console.WriteLine("Digite a frase: ");
+            string frase = Console.ReadLine();
+
+            frase = frase.ToLower();
+
+            string[] fraseArray = frase.Split(' ');
+
+            foreach(string s in fraseArray)
+            {
+                if (fraseArray.Contains(s))
+                {
+                    dic[s]++;
+                }
+                else
+                {
+                    dic[s] = 1;
+                }
+            }
+
+            foreach (var kvp in dic)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} ocorrência(s)");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
